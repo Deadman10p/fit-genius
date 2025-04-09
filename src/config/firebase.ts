e574383531c126b28c7542958713f310d66cdee1
Deadman10p <bulegafarid@gmail.com>
2025-04-09 17:13:01 +0300

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -16,9 +16,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyC_3mO1MJEoVNbhO9SeC5bFD6UiNWWo__M",
   authDomain: "fit-genius-f6318.firebaseapp.com",
   projectId: "fit-genius-f6318",
-  storageBucket: "fit-genius-f6318.appspot.com",
+  storageBucket: "fit-genius-f6318.firebasestorage.app",
   messagingSenderId: "350597158093",
-  appId: "1:350597158093:web:54529a4846348b03cb4584"
+  appId: "1:350597158093:web:54529a4846348b03cb4584",
+  measurementId: "G-RY8VTQFGQS"
 };
 
 // Initialize Firebase
