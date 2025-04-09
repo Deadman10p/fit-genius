3f8ca996b55763aa4ff1b6795334dc5f15695ef3
Deadman10p <bulegafarid@gmail.com>
2025-04-09 17:19:21 +0300

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { 
   getAuth, 
   signInWithEmailAndPassword, 
@@ -24,6 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
 // Enable Firebase Auth persistence to keep users logged in
